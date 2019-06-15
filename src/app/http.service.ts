c17b5e342626454f2ce417c746739c851d6f8beb
s16315 <s16315@pjwstk.edu.pl>
2019-06-15 16:34:51 +0200

    v2

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,13 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {delay} from 'rxjs/operators';
+
+
+interface Config {
+  key: string;
+  name: string;
+}
 
 @Injectable()
 export class HttpService {
@@ -10,7 +18,9 @@ export class HttpService {
     return this.http.get('assets/names.json');
   }
 
-  getConfig() {
-    return this.http.get('assets/config.json');
+  getConfig(): Observable<Config> {
+    return this.http.get<Config>('assets/config.json').pipe(
+      delay(2000)
+    );
   }
 }
